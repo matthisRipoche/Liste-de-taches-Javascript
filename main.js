aa9a9f238b402d23f8e40d008f4f5d6e5fbe113a
matthisRipoche <matthisripoche@gmail.com>
2024-02-28 13:37:08 +0100

    ok

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Code
+const myUL = document.getElementById("myUL");
+const Li = document.getElementsByName('li');
+
+function newElement(){
+    const newLi = document.createElement('li');
+    
+    const input = document.getElementById("input").value;
+    console.log(input);
+
+    newLi.innerHTML = input;
+    myUL.appendChild(newLi);
+
+    console.log(Li);
+}
+
